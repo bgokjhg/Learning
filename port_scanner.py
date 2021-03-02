b4ab8ab6355e8114b83e1048c86b861a857deec2
bgokjhg <50530478+bgokjhg@users.noreply.github.com>
2021-03-02 21:57:22 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -14,15 +14,29 @@ def scan(target,port):
 		else:
 			return False
 
+def check_ip(target):
+	if target == 'localhost':
+		return True
+	else:
+		r = re.search('[0-9]{1,3}(\\.[0-9]{1,3}){1,3}',target)
+		if r:
+			return True
+		else:
+			print("Please use a valid ip address")
+			return False
+
 def main():
 	parser = argparse.ArgumentParser(description="Scanning ports of domains")
 	parser.add_argument('-i','--ip',help='Enter ip to scan')
 	args = parser.parse_args()
 	targ = args.ip
-	if args.ip != None:
-		for i in range(65535):
-			if scan(targ,i):
-				print(i,"is open")
+	if targ != None:
+		if check_ip(targ):
+			for i in range(65535):
+				if scan(targ,i):
+					print(i,"is open")
+		else:
+			print("Use a valid ip address")
 	else:
 		print("Use ./port_scanner.py -h")
 
